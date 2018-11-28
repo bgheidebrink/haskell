a5ff78155154e40400bc3a649167a92c1c59a6f6
Benjamin Heidebrink <bgheidebrink@gmail.com>
2018-11-28 14:15:58 -0800

    Complete Log

--- log-file-parsing/src/LogAnalysis.hs
+++ log-file-parsing/src/LogAnalysis.hs
@@ -23,31 +23,36 @@ parse :: String -> [LogMessage]
 parse s = map parseMessage (lines s)
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (LogMessage nc nt ns) (Node lnode (LogMessage c t s) rnode)
-  | nt < t && lnode == Leaf   = Node
+insert (LogMessage nc nt ns) (Node left (LogMessage c t s) right)
+  | nt < t && left == Leaf   = Node
                                   (Node Leaf (LogMessage nc nt ns) Leaf)
                                   (LogMessage c t s)
-                                  rnode
-  | nt > t && rnode == Leaf   = Node
-                                  lnode
+                                  right
+  | nt > t && right == Leaf   = Node
+                                  left
                                   (LogMessage c t s)
                                   (Node Leaf (LogMessage nc nt ns) Leaf)
   | nt < t                    = Node
-                                  (insert (LogMessage nc nt ns) lnode)
+                                  (insert (LogMessage nc nt ns) left)
                                   (LogMessage c t s)
-                                  rnode
+                                  right
   | nt > t                    = Node
-                                  lnode
+                                  left
                                   (LogMessage c t s)
-                                  (insert (LogMessage nc nt ns) rnode)
+                                  (insert (LogMessage nc nt ns) right)
 insert lm Leaf = Node Leaf lm Leaf
 insert _ tree = tree
 
 build :: [LogMessage] -> MessageTree
-build = foldr insert Lea   f
--- build [] = Leaf
--- build (x:xs) = insert x (build xs)
+build = foldr insert Leaf
 
--- inOrder :: MessageTree -> [LogMessage]
--- inOrder Node lnode logMessage rnode
---   | lnode == Node
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left logMessage right) =
+  inOrder left ++ [logMessage] ++ inOrder right
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong (LogMessage (Error a) _ s : xs)
+  | a > 50 = s : whatWentWrong xs
+whatWentWrong (_:xs) = whatWentWrong xs
